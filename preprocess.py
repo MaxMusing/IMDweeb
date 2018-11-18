a2812811e3b45e9c74f3f4c3c5a5edd8aa5b5013
Sarah Fernandez <sarahnfdez@gmail.com>
2018-11-17 22:45:45 -0500

    commented my section -sf

--- preprocess.py
+++ preprocess.py
@@ -44,24 +44,40 @@ for movie in json_data:
 
 #sorted(words, key=words.get, reverse=True)
 vocabulary =[k.lower() for k in sorted(words, key=words.get, reverse=True)]
+#index variable for x
 j = 0
+#for each story in x
 for story in x:
+   #converts each token to lowercase, removes stopwords and punctuation
    story = [w.lower() for w in story if not w in stop_words and w not in punc]
+   #for index in story
    for i in range(len(story)):
+       #get the word at the index
        word = story[i]
+       #find the index of the word in the vocabulary list (word frequency)
        ind = vocabulary.index(word)
+       #increment by 1 to get true value
        ind += 1
+       #replace the word with its word frequency value
        story[i] = ind
+   #replace the value at index j 
    x[j] = story
+   #increment index
    j += 1
  
+#set maxsize = 0    
 maxsize = 0
+#for each story in x
 for story in x:
+    #get the length of each story (# of words)
     size = len(story)
+    #if size is bigger than maxsize, maxsize becomes size
     if size > maxsize:
         maxsize = size
 
+#for each story in x
 for story in x:
+    #pads each story with 0s if it is not the maxsize
     story += [0]*(maxsize - len(story))
         
         
