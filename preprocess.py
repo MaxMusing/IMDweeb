3144b1cb52a13ecb7aedd8965dc0c3c4df74d04c
Sarah Fernandez <sarahnfdez@gmail.com>
2018-11-17 22:21:55 -0500

    hi

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,6 @@
 import json
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 json_file = open('data.json')
 json_string = json_file.read()
@@ -8,22 +10,25 @@ x = []
 y = []
 
 words = {}
+punc = ['.', '?', '!', ',', '...', '\'ll', '\'ve', ')', '\'\'', '``', '(', '-', 'n\'t', '\'s', ';',':']
+stop_words = set(stopwords.words("english"))
 
 for movie in json_data:
     storyline = movie['storyline']
     if not storyline:
         continue
-    storyline = storyline.strip().split()
+    storyline = word_tokenize(storyline.strip().lower())
 
     rating = movie['imdb_score'][0]
     if not rating:
         continue
 
     for word in storyline:
-        if word in words:
-            words[word] += 1
-        else:
-            words[word] = 1
+        if word not in punc and word not in stop_words:
+            if word in words:
+                words[word] += 1
+            else:
+                words[word] = 1
 
     x.append(storyline)
     y.append(rating)
@@ -35,8 +40,28 @@ print('OUTPUT')
 print(y[:20])
 
 print('WORDS')
-print(words)
+#print(words)
 
 #sorted(words, key=words.get, reverse=True)
-vocabulary =[(k, words[k]) for k in sorted(words, key=words.get, reverse=True)]
+vocabulary =[k.lower() for k in sorted(words, key=words.get, reverse=True)]
+j = 0
+for story in x:
+   story = [w.lower() for w in story if not w in stop_words and w not in punc]
+   for i in range(len(story)):
+       word = story[i]
+       ind = vocabulary.index(word)
+       ind += 1
+       story[i] = ind
+   x[j] = story
+   j += 1
+ 
+maxsize = 0
+for story in x:
+    size = len(story)
+    if size > maxsize:
+        maxsize = size
 
+for story in x:
+    story += [0]*(maxsize - len(story))
+        
+        
